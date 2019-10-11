cad18c1b0c27f8da608008d8e603b26672244822
b1zz3 <donhager23@gmail.com>
2019-10-10 22:51:51 -0400

    Work on stretch goals, random num method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -40,6 +40,7 @@ class Instructor extends Person {
     }
     demo(subject) {return `Today we ar learning about ${subject}`}
     grade(student, subject) {return `${student.name} receives a perfect score on ${subject}`}
+    gradeTroll(student) {return (student.grade - Math.floor((Math.random() * 100) + 1)) }
 }
 
 // #### Student
@@ -60,6 +61,7 @@ class Student extends Person {
         this.previousBackground = stuAttr.previousBackground;
         this.className = stuAttr.className;
         this.favSubjects = stuAttr.favSubjects;
+        this.grade = stuAttr.grade;
     }
     listsSubjects() {return this.favSubjects.map(subject)}
     PRAssignment(subject) {return `${this.name} has submitted a PR for ${subject}`}
@@ -76,7 +78,6 @@ class Student extends Person {
 // * ProjectManagers have the following Methods:
 //   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
 //   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-
 class ProjectManager extends Instructor {
     constructor(projAttr) {
         super(projAttr);
@@ -87,16 +88,55 @@ class ProjectManager extends Instructor {
     debugCode(student, subject) {console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)}
 }
 
-
-
-const fred = new ProjectManager({
-    name: 'Fred',
-    location: 'Bedrock',
-    age: 37,
+const sterlingArcher = new ProjectManager({
+    name: 'Archer',
+    location: 'New York',
+    age: 35,
     favLanguage: 'JavaScript',
     specialty: 'Front-end',
-    catchPhrase: `Don't forget the homies`
+    catchPhrase: `Oh I'm sorry, I can't hear you over THE SOUND OF HOW AWESOME I AM!`
   });
 
-  console.log(fred);
-  console.log(fred.standUp('myTeam'));
+const lana = new Instructor({
+    name: 'Lana',
+    location: 'New York',
+    age: 28,
+    favLanguage: 'Python',
+    speciality: 'Back-end',
+    catchPhrase: `Cuase that's how you get ants!`
+});
+
+const cyril = new Student({
+    name: 'Cyril',
+    location: 'New York',
+    age: 33,
+    favLanguage: 'Basic',
+    speciality: 'Data Base',
+    catchPhrase: `I happen to be a kickass accountant!`,
+    grade: 80
+});
+
+const cheryl = new Student({
+    name: 'Cheryl',
+    location: 'New York',
+    age: 25,
+    favLanguage: 'wingdings',
+    speciality: 'Turning it off and back on again',
+    catchPhrase: `Where ISN'T the bathroom? Am I right?`,
+    grade: 80
+});
+
+
+  console.log(sterlingArcher);
+  sterlingArcher.standUp('Team Archer');
+  console.log(lana);
+  console.log(cheryl);
+  console.log(cyril);
+
+  console.log(lana.gradeTroll(cheryl));
+
+//   * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+// * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
+// * Add a graduate method to a student.
+//   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
+//   * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
